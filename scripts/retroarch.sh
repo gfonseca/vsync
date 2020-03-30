2b9be7ec09b86223f74e1556b33538ecad4bbf8a
Georgio Barbosa <georgio.barbosa@gmail.com>
2020-03-30 13:05:33 -0300

    feat: retroarch module

---
+++ scripts/retroarch.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+install_retroarch(){
+	RA_REPO_URL=$1
+	RA_CLONE_DIR=$2
+
+	echo $ES_ClONE_FOLDER
+	echo "> Instaling LibRetro"
+	echo "> Cloning LibRetro"
+
+	rm -rf $RA_CLONE_DIR
+	git clone $RA_REPO_URL $RA_CLONE_DIR
+
+	cd $RA_CLONE_DIR
+	./configure
+	if [ $? -ne 0 ]; then
+		echo "Error configuring Retroarch"
+		return 1
+	fi 
+
+	make clean && make -j4
+	if [ $? -ne 0 ]; then
+		echo "Error building Retroarch"
+		return 1
+	fi 
+
+	cd $RA_CLONE_DIR && chmod +x ./retroarch && sudo cp -rfv ./retroarch /usr/local/bin/retroarch
+	rm -rf $RA_CLONE_DIR
+
+	echo "> Retroarch done"
+}
