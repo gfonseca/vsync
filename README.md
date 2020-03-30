c10e7619bb518929e74f74a373d3c5ad77276d97
Georgio Barbosa <georgio.barbosa@gmail.com>
2020-03-30 13:05:05 -0300

    Adding install.sh base script and Readme.md

---
+++ README.md
@@ -0,0 +1,12 @@
+# Retroarch + Emulation Station install Script
+
+## Automatic install and configure retroarch + Emulation Station
+
+### Run
+```sh
+$ git clone github.com/gfonseca/vsync
+$ cd vsync
+$ chmod +x install.sh
+$ emulationstation
+```
+
---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+BASEDIR=$(dirname $(realpath "$0"))
+
+ES_REPO_URL=https://github.com/Aloshi/EmulationStation
+ES_CLONE_DIR=/tmp/emulation-station
+ES_CONFIG_DIR=$HOME/.emulationstation/
+ES_THEME_DIR=$HOME/.emulationstation/themes
+
+PIXEL_THEME_DIR=$ES_THEME_DIR/pixel/
+PIXEL_REPO_URL=https://github.com/RetroPie/es-theme-pixel
+
+RA_REPO_URL=https://github.com/libretro/RetroArch.git
+RA_CLONE_DIR=/tmp/libretro
+RA_CONF_DIR=$HOME/.config/retroarch
+RA_CORE_DIR=$RA_CONF_DIR/cores/
+
+PICO_REPO_URL=https://github.com/libretro/picodrive
+PICO_CLONE_DIR=/tmp/picodrive/
+
+source $BASEDIR/scripts/emulationstation.sh
+source $BASEDIR/scripts/retroarch.sh
+source $BASEDIR/scripts/pixel-theme.sh
+source $BASEDIR/scripts/picodrive.sh
+
+echo "Emulation Station Script + retroarch configuration script"
+echo "Starting instalation" 
+
+
+#echo "Instaling Emulation Station"
+#install_emulationstation $BASEDIR $ES_REPO_URL $ES_CLONE_DIR $ES_CONFIG_DIR $ES
+#if [ $? -ne 0 ]; then
+#    echo "Failed to install Emulaton Station"
+#    exit 1
+#fi
+
+#echo "Instaling Pixel Theme"
+#install_pixel_theme $PIXEL_REPO_URL $PIXEL_THEME_DIR
+#if [ $? -ne 0 ]; then
+#    echo "Failed to install Pixel Theme"
+#    exit 1
+#fi
+
+#echo "Instaling Retroarch"
+#install_retroarch $RA_REPO_URL $RA_CLONE_DIR
+#if [ $? -ne 0 ]; then
+#    echo "Failed to install Retroarch"
+#    exit 1
+#fi
+
+echo "Instaling Retroarch core Picodrive"
+install_picodrive  $PICO_REPO_URL $PICO_CLONE_DIR $RA_CORE_DIR
+if [ $? -ne 0 ]; then
+    echo "Failed to install Picodrive"
+    exit 1
+fi
